540aad4a236baf2cee0c442f1d59b3c66426226c
Janet Leon <ajleon00@gmail.com>
2020-02-09 23:46:27 -0800

    sanity check- testing config of server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
+
 @NgModule({
   declarations: [AppComponent, ServerComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
